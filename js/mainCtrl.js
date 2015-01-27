6f0e5a0b7fb6c8feb75424cea56cfb4a9eed28db
Jeff Chapman <jeffcchapman@gmail.com>
2015-01-27 13:03:53 -0700

    finished mini project

---
+++ js/mainCtrl.js
@@ -0,0 +1,8 @@
+var app = angular.module('app');
+
+app.controller('mainCtrl', function($scope) {
+
+$scope.name = "Jeff Chapman";
+
+
+})
---
+++ js/timeDirective.js
@@ -0,0 +1,35 @@
+var app = angular.module('app');
+
+app.directive('showTime', function() {
+
+	return {
+		restrict: 'E',
+		template: '<div> The current time is {{time}} </div>',
+		link: function(scope, element, attrs) {
+			var currentTime = new Date();
+			scope.time = currentTime.toString();
+		}
+	}
+
+});
+
+app.directive('', ['', function(){
+	// Runs during compile
+	return {
+		// name: '',
+		// priority: 1,
+		// terminal: true,
+		// scope: {}, // {} = isolate, true = child, false/undefined = no change
+		// controller: function($scope, $element, $attrs, $transclude) {},
+		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		// restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+		// template: '',
+		// templateUrl: '',
+		// replace: true,
+		// transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+		link: function($scope, iElm, iAttrs, controller) {
+			
+		}
+	};
+}]);
